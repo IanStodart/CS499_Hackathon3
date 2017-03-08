729b5d5d3b542f9656bce63909532009135576cd
nliupeng <nliupeng@gmail.com>
2017-03-07 18:37:03 -0800

    html functions update

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 var app = angular.module('app', ['ngMaterial']);
 
-app.controller('AppCtrl', ['$scope', function($scope){
+app.controller('appController', ['$scope', '$http', function($scope, $http){
 
+	$scope.test = "";
+	$scope.record = {}
+	$scope.request = {}
 
-  $scope.myFunction = function() {
+	$scope.submitRecord = function() {
+		$http.post("/save", {params:{"road": $scope.record.road, 
+									"direction": $scope.record.direction, 
+									"day": $scope.record.day, 
+									"time": $scope.record.time, 
+									"status": $scope.record.status}})
+			.then(function mySuccess(response) {
+		        $scope.test = response.data;
+		    }, function myError(response) {
+		        $scope.test = response.statusText;
+		    });
+	}
 
-  };
+	$scope.predictRequest = function() {
+	}
+
  
 }]);
